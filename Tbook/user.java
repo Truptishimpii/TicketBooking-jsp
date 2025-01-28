768c99125782df51ccbc0a48c7ccd042ed0a6992
Truptishimpii <163382610+Truptishimpii@users.noreply.github.com>
2025-01-28 09:58:33 +0530

    Create user.java

---
+++ Tbook/user.java
@@ -0,0 +1,45 @@
+package Tbook;
+
+public class User {
+	
+	private int id;
+	private String username,destination,date,quantity,gender;
+	public int getId() {
+	 return id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+
+}
